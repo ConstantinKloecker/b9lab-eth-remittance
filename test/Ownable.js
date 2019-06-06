8ed7e83a3513b6c887eb68e3e1a1de45c01c73dc
ConstantinKloecker <github@kloecker.io>
2019-06-06 15:41:44 +0200

    Uncommented tests

--- test/Ownable.js
+++ test/Ownable.js
@@ -1,29 +1,29 @@
-// const Remittance = artifacts.require("Remittance");
-// const truffleAssert = require("truffle-assertions");
+const Remittance = artifacts.require("Remittance");
+const truffleAssert = require("truffle-assertions");
 
-// contract("Testing Ownable features of Remittance contract", accounts => {
-//     let instance;
-//     const [owner, alice, bob, carol] = accounts;
+contract("Testing Ownable features of Remittance contract", accounts => {
+    let instance;
+    const [owner, alice, bob, carol] = accounts;
 
-//     beforeEach("Deploying clean Remittance contract", async () => {
-//         instance = await Remittance.new({ from: owner });
-//     });
+    beforeEach("Deploying clean Remittance contract", async () => {
+        instance = await Remittance.new({ from: owner });
+    });
 
-//     it("Deployer is contract owner", async () => {
-//         assert.equal(await instance.getOwner({ from: owner }), owner, "Deployer should be the owner");
-//     });
+    it("Deployer is contract owner", async () => {
+        assert.equal(await instance.getOwner({ from: owner }), owner, "Deployer should be the owner");
+    });
 
-//     it("Owner can transfer ownership", async () => {
-//         let ownership = await instance.changeOwner(alice, { from: owner });
-//         assert.equal(await instance.getOwner({ from: owner }), alice, "Alice should be the new owner");
-//         truffleAssert.eventEmitted(ownership, "LogNewOwner", (ev) => {
-//             return ev.oldOwner === owner && ev.newOwner === alice;
-//         });
-//     });
+    it("Owner can transfer ownership", async () => {
+        let ownership = await instance.changeOwner(alice, { from: owner });
+        assert.equal(await instance.getOwner({ from: owner }), alice, "Alice should be the new owner");
+        truffleAssert.eventEmitted(ownership, "LogNewOwner", (ev) => {
+            return ev.oldOwner === owner && ev.newOwner === alice;
+        });
+    });
 
-//     it("Non-owner can not transfer ownership", async () => {
-//         await truffleAssert.fails(
-//             instance.changeOwner(alice, { from: alice })
-//         );
-//     });
-// });
+    it("Non-owner can not transfer ownership", async () => {
+        await truffleAssert.fails(
+            instance.changeOwner(alice, { from: alice })
+        );
+    });
+});
